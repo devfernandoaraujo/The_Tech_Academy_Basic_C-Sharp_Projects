1309f38c5bd0b129456478c2e9ea6eb0692f5785
devfernandoaraujo <fernandomatosaraujo@gmail.com>
2023-06-29 14:28:39 -0700

    Adding Operators_Assignment

---
+++ Operators_Assignment/Employee .cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators_Assignment
+{
+    internal class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get;set; }
+        public string LastName { get;set; }
+
+
+        public static bool operator ==(Employee employee1, Employee employee2)
+        {
+            if (ReferenceEquals(employee1, employee2))
+            {
+                return true;
+            }
+
+            if (ReferenceEquals(employee1, null) || ReferenceEquals(employee2, null))
+            {
+                return false;
+            }
+
+            return employee1.Id == employee2.Id;
+        }
+
+        public static bool operator !=(Employee employee1, Employee employee2)
+        {
+            return !(employee1 == employee2);
+        }
+    }
+}
---
+++ Operators_Assignment/Program.cs
@@ -0,0 +1,13 @@
+﻿//Refering the Namespace where the class is located
+using Operators_Assignment;
+
+//List of Employees
+Employee employee1 = new Employee { Id=1, FirstName="Fernando", LastName="Araujo"};
+Employee employee2 = new Employee { Id = 2, FirstName = "Luiz", LastName = "Roxo" };
+Employee employee3 = new Employee { Id = 1, FirstName = "Fernando", LastName = "Araujo" };
+
+//Comaring Employees
+Console.WriteLine("Is the Employee 1 equals the Employee 2? {0}", employee1 == employee2);
+Console.WriteLine("Is the Employee 1 equals the Employee 3? {0}", employee1 == employee3);
+
+Console.ReadLine();
