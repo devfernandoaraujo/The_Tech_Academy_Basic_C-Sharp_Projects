765ed9ede877e76ca72b1ff3ae4404e499f23a1b
devfernandoaraujo <fernandomatosaraujo@gmail.com>
2023-06-29 13:56:50 -0700

    Adding Method Class Assignment Projet

---
+++ Method_Class_Assignment/Calc_Math.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Class_Assignment
+{
+    internal class Calc_Math
+    {
+        public void Calc(int x, int y)
+        {
+            x += 30;
+            Console.WriteLine("This is the value of y: {0}", y);
+        }
+    }
+}
---
+++ Method_Class_Assignment/Program.cs
@@ -0,0 +1,10 @@
+﻿//Referiing the Namespace where the Calc_Math is located.
+using Method_Class_Assignment;
+
+//Instance of the Class Calc_Math
+Calc_Math myCalc = new Calc_Math();
+
+//Calling the and passing paramters for the Calc method
+myCalc.Calc( x:25, y: 6);
+
+//Atention: This is using .NET 6 Framework so there is no need to declare the Main mmethod anymore  
